36a060ea37308017966158daa203144cab3a9700
OmarA <44711508+OmarA-22C@users.noreply.github.com>
2021-11-12 17:12:37 -0800

    Created BinarySearchTree.h

--- src/BinarySearchTree.h
+++ src/BinarySearchTree.h
@@ -1,11 +1,141 @@
-#ifndef BINARY_SEARCH_TREE_H
-#define BINARY_SEARCH_TREE_H
+#pragma once
+#pragma once
+// Binary Search Tree ADT
+// Created by A. Student
+// Modified by:  
 
+#ifndef _BINARY_SEARCH_TREE
+#define _BINARY_SEARCH_TREE
+//#include "College.h"
 #include "BinaryTree.h"
 
-template <class T>
+template<class T>
 class BinarySearchTree : public BinaryTree<T>
 {
+public:
+    // insert a node at the correct location
+    bool insert(const T& item);
+    // remove a node if found
+    //bool remove(const T &item);
+    // find a target node
+    bool search(const T& target, T& returnedItem) const;
+    // find the smallest node
+    bool findSmallest(T& returnedItem) const;
+    // find the largest node
+    bool findLargest(T& returnedItem) const;
+
+private:
+    // internal insert node: insert newNode in nodePtr subtree
+    BinaryNode<T>* _insert(BinaryNode<T>* nodePtr, BinaryNode<T>* newNode);
+
+    // search for target node
+    BinaryNode<T>* _search(BinaryNode<T>* treePtr, const T& target) const;
+
+    // find the smallest node
+    BinaryNode<T>* _findSmallest(BinaryNode<T>* nodePtr, T& smallest) const;
+
+    // find the largest node
+    BinaryNode<T>* _findLargest(BinaryNode<T>* nodePtr, T& smallest) const;
+
+    // internal remove node: locate and delete target node under nodePtr subtree
+    // BinaryNode<T>* _remove(BinaryNode<T>* nodePtr, const T target, bool &success);
+
+     // delete target node from tree, called by internal remove node
+     //BinaryNode<T>* _removeNode(BinaryNode<T>* targetNodePtr);
+
+     // remove the leftmost node in the left subtree of nodePtr
+     //BinaryNode<T>* _removeLeftmostNode(BinaryNode<T>* nodePtr, T &successor);
+
 };
+
+
+///////////////////////// public function definitions ///////////////////////////
+//Wrapper for _insert - Inserting items within a tree
+template<class T>
+bool BinarySearchTree<T>::insert(const T& newEntry)
+{
+    BinaryNode<T>* newNodePtr = new BinaryNode<T>(newEntry);
+    this->rootPtr = _insert(this->rootPtr, newNodePtr);
+    return true;
+}
+
+//Wrapper for _search
+// - it calls the private _search function that returns a Node pointer or NULL
+// - if found, it copies data from that node and sends it back to the caller 
+//   via the output parameter, and returns true, otherwise it returns false.
+template<class T>
+bool BinarySearchTree<T>::search(const T& anEntry, T& returnedItem) const
+{
+    BinaryNode<T>* temp = nullptr;
+    /* Write your code here */
+    temp = _search(this->rootPtr, anEntry);
+    if (!temp) {
+        return false;
+    }
+    returnedItem = temp->getItem();
+    return true;
+
+}
+
+
+//////////////////////////// private functions ////////////////////////////////////////////
+
+//Implementation of the insert operation
+template<class T>
+BinaryNode<T>* BinarySearchTree<T>::_insert(BinaryNode<T>* nodePtr,
+    BinaryNode<T>* newNodePtr)
+{
+    if (!nodePtr && this->isEmpty()) // == NULL
+    {
+        nodePtr = newNodePtr;
+        this->count++;
+        return nodePtr;
+    }
+    if (nodePtr) {
+        if (nodePtr->getItem() > newNodePtr->getItem()) {
+            if (!_insert(nodePtr->getLeftPtr(), newNodePtr)) {
+                nodePtr->setLeftPtr(newNodePtr);
+                this->count++;
+            }
+        }
+        else {
+            if (!_insert(nodePtr->getRightPtr(), newNodePtr)) {
+                nodePtr->setRightPtr(newNodePtr);
+                this->count++;
+            }
+        }
+    }
+    return nodePtr;
+}
+
+//Implementation for the search operation
+// - return NULL if target not found, otherwise
+// - returns a pointer to the node that matched the target
+template<class T>
+BinaryNode<T>* BinarySearchTree<T>::_search(BinaryNode<T>* nodePtr,
+    const T& target) const
+{
+    BinaryNode<T>* found = nullptr;
+
+    /* Write your code here */
+
+    if (nodePtr == nullptr) {
+        return found;
+    }
+
+    if (nodePtr->getItem() > target) {
+        return _search(nodePtr->getLeftPtr(), target);
+    }
+    else {
+        if (nodePtr->getItem() < target) {
+            return _search(nodePtr->getRightPtr(), target);
+        }
+        else {
+            found = nodePtr;
+            return found;
+        }
+    }
+    //return found;
+}
 
 #endif
--- src/BinaryTree.h
+++ src/BinaryTree.h
@@ -1,11 +1,142 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
+#pragma once
+#pragma once
+// Binary tree abstract base class
+// Created by A. Student
+// Modified by:
+
+#ifndef _BINARY_TREE
+#define _BINARY_TREE
 
 #include "BinaryNode.h"
 
-template <class T>
+
+template<class T>
 class BinaryTree
 {
+protected:
+	BinaryNode<T>* rootPtr;		// ptr to root node
+	int count;							// number of nodes in tree
+
+public:
+	// "admin" functions
+	BinaryTree() { rootPtr = 0; count = 0; }
+	BinaryTree(const BinaryTree<T>& tree) { }
+	virtual ~BinaryTree() { destroyTree(rootPtr); }
+
+	// common functions for all binary trees
+	bool isEmpty() const { return count == 0; }
+	int getCount() const { return count; }
+	void clear() { destroyTree(rootPtr); rootPtr = 0; count = 0; }
+	void preOrder(void visit(T&)) const { _preorder(visit, rootPtr); }
+	void inOrder(void visit(T&)) const { _inorder(visit, rootPtr); }
+	void postOrder(void visit(T&)) const { _postorder(visit, rootPtr); }
+	void printTree(void visit(T&, int)) const { _printTree(visit, rootPtr, 1); }
+	void printLeafNodes(void visit(T&)) const { _printLeafNodes(visit, rootPtr); }
+
+	// abstract functions to be implemented by derived class
+	virtual bool insert(const T& newData) = 0;
+	//virtual bool remove(const T &data) = 0;
+	virtual bool search(const T& target, T& returnedItem) const = 0;
+
+private:
+	// delete all nodes from the tree
+	void destroyTree(BinaryNode<T>* nodePtr);
+
+	// internal traverse
+	void _preorder(void visit(T&), BinaryNode<T>* nodePtr) const;
+	void _inorder(void visit(T&), BinaryNode<T>* nodePtr) const;
+	void _postorder(void visit(T&), BinaryNode<T>* nodePtr) const;
+	void _printTree(void visit(T&, int), BinaryNode<T>* nodePtr, int level) const;
+	void _printLeafNodes(void visit(T&), BinaryNode<T>* rootPtr) const;
 };
 
+//Destroy the entire tree
+template<class T>
+void BinaryTree<T>::destroyTree(BinaryNode<T>* nodePtr)
+{
+	if (nodePtr) // != NULL
+	{
+		destroyTree(nodePtr->getLeftPtr());
+		destroyTree(nodePtr->getRightPtr());
+		//cout << "DEBUG - Destructor: Now deleting " << nodePtr->getItem().getName() << endl;
+		delete nodePtr;
+	}
+}
+
+//Preorder Traversal
+template<class T>
+void BinaryTree<T>::_preorder(void visit(T&), BinaryNode<T>* nodePtr) const
+{
+	/* Write your code here */
+	if (nodePtr) // != NULL    // added by Omar
+	{
+		T item = nodePtr->getItem();
+		visit(item);
+		_preorder(visit, nodePtr->getLeftPtr());
+		_preorder(visit, nodePtr->getRightPtr());
+	}
+
+}
+
+//Inorder Traversal
+template<class T>
+void BinaryTree<T>::_inorder(void visit(T&), BinaryNode<T>* nodePtr) const
+{
+	if (nodePtr) // != NULL
+	{
+		T item = nodePtr->getItem();
+		_inorder(visit, nodePtr->getLeftPtr());
+		visit(item);
+		_inorder(visit, nodePtr->getRightPtr());
+	}
+}
+
+//Postorder Traversal
+template<class T>
+void BinaryTree<T>::_postorder(void visit(T&), BinaryNode<T>* nodePtr) const
+{
+	/* Write your code here */
+	if (nodePtr) {// added by Omar
+		T item = nodePtr->getItem();
+		_postorder(visit, nodePtr->getLeftPtr());
+		visit(item);
+		_postorder(visit, nodePtr->getRightPtr());
+	}
+}
+
+//Prints tree as an indented list
+template<class T>
+void BinaryTree<T>::_printTree(void visit(T&, int), BinaryNode<T>* nodePtr, int level) const
+{
+	/* Write your code here */
+	if (nodePtr) // != NULL  // added by Omar
+	{
+		T item = nodePtr->getItem();
+		visit(item, level);
+		level++;
+		_printTree(visit, nodePtr->getRightPtr(), level);
+		_printTree(visit, nodePtr->getLeftPtr(), level);
+	}
+}
+
+template<class T>
+void BinaryTree<T>::_printLeafNodes(void visit(T&), BinaryNode<T>* nodePtr) const {
+
+	if (!nodePtr) // != NULL
+		return;
+
+	if (nodePtr->isLeaf())
+	{
+		T item = nodePtr->getItem();
+		visit(item);
+		return;
+	}
+
+	if (nodePtr->getLeftPtr())
+		_printLeafNodes(visit, nodePtr->getLeftPtr());
+	if (nodePtr->getRightPtr())
+		_printLeafNodes(visit, nodePtr->getRightPtr());
+}
+
 #endif
+
