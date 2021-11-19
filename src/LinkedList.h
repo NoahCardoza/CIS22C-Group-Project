3fd80304e8573fe686923b91e496fd50add04fba
NoahCardoza <noahcardoza@gmail.com>
2021-11-19 11:12:59 -0800

    fixed(LinkedList): compile errors

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -10,63 +10,64 @@
 
 #include "HashNode.h"
 
-template <class ItemType>
+template <class T>
 class LinkedList
 {
 private:
-    HashNode <ItemType>* head;
+    HashNode<T> *head;
     int length;
-    
+
 public:
     LinkedList();  //constructor
     ~LinkedList(); //destructor
-    
+
     //Linked List Operations
-    int getLength() const {return length;}
-    void insertNode(const ItemType*);
-    bool deleteNode(const ItemType*, ItemType* );
-    
+    int getLength() const { return length; }
+    void insertNode(const T *);
+    bool deleteNode(const T *, T **);
+
     void displayList() const;
-    bool searchList(const ItemType*, ItemType*) const;
+    bool searchList(const T *, T **) const;
 };
 
-template <class ItemType>
-LinkedList <ItemType>::LinkedList()
+template <class T>
+LinkedList<T>::LinkedList()
 {
-    head = new HashNode<ItemType>; //head points to the sentinel node
+    head = new HashNode<T>; //head points to the sentinel node
     head->setNext(NULL);
     length = 0;
 }
 
 //The insertNode function inserts a new node in a sorted linked list
-void LinkedList<ItemType>::insertNode(const ItemType* dataIn)
+template <class T>
+void LinkedList<T>::insertNode(const T *dataIn)
 {
-    HashNode<ItemType> *newNode; //A new node
-    HashNode<ItemType> *pCur; //To traverse the list
-    HashNode<ItemType> *pPre; //The previous node
-    
+    HashNode<T> *newNode; //A new node
+    HashNode<T> *pCur;    //To traverse the list
+    HashNode<T> *pPre;    //The previous node
+
     //Allocate a new node and store the pointer to object there
-    newNode = new HashNode<ItemType>;
+    newNode = new HashNode<T>;
     newNode->setItem(dataIn);
-    
+
     //Initialize pointers
     pPre = head;
     pCur = head->getNext();
-    
+
     //Find Location: skip all nodes whose ID is less than the dataIn->ID
-    while (pCur && newNode->getItem()->getID() > pCur->getItem()->getID())
+    while (pCur && newNode->getItem() > pCur->getItem())
     {
         pPre = pCur;
         pCur = pCur->getNext();
     }
-    
+
     //Insert the new node between pPre and pCur
     pPre->setNext(newNode);
     if (pCur)
     {
         newNode->setNext(pCur);
     }
-    
+
     //Update the counter
     length++;
 }
@@ -76,37 +77,36 @@ void LinkedList<ItemType>::insertNode(const ItemType* dataIn)
 // in a sorted linked list; if found, the node is
 // deleted from the list and from memory.
 //**************************************************
-template <class ItemType>
-bool LinkedList<Itemtype>::deleteNode(const ItemType* target, Itemtype** itemOut)
+template <class T>
+bool LinkedList<T>::deleteNode(const T *target, T **itemOut)
 {
-    HashNode<ItemType> *pCur; //to traverse the lsit
-    HashNode<ItemType> *pPre; //to point to the previous node
+    HashNode<T> *pCur; //to traverse the lsit
+    HashNode<T> *pPre; //to point to the previous node
     bool deleted = false;
-    
+
     //Initialize pointers
     pCur = head->getNext();
     pPre = head;
-    
+
     //Find node containing the target: Skip all nodes whose ID is less than the target
-    while(pCur && pCur->getItem()->getID() < target->getID())
+    while (pCur && pCur->getItem() < target)
     {
         pPre = pCur;
         pCur = pCur->getNext();
     }
-    
+
     // If found, delete the node
-    if (pCur && pCur->getItem()->getID() == target.getID())
+    if (pCur && pCur->getItem() == target)
     {
         pPre->setNext(pCur->getNext());
         *itemOut = pCur->getItem();
-        
+
         delete pCur;
         deleted = true;
         length--;
     }
-    
-    return deleted;
 
+    return deleted;
 }
 
 //**************************************************
@@ -114,23 +114,23 @@ bool LinkedList<Itemtype>::deleteNode(const ItemType* target, Itemtype** itemOut
 // stored in each node of the linked list
 // pointed to by head, except the sentinel node
 //**************************************************
-template <class ItemType>
-void LinkedList<ItemType>::displayList() const
+template <class T>
+void LinkedList<T>::displayList() const
 {
-     HashNode<ItemType> *pCur;  // To move through the list
+    HashNode<T> *pCur; // To move through the list
 
-     // Position pCur: skip the head of the list.
-     pCur = head->getNext();
+    // Position pCur: skip the head of the list.
+    pCur = head->getNext();
 
-     // While pCur is not NULL, traverse the list.
-     while (pCur)
-     {
-         // Display the value in this node.
-         
-          std::cout << pCur->getItem();
-         
-         // Move to the next node.
-         pCur = pCur->getNext();
+    // While pCur is not NULL, traverse the list.
+    while (pCur)
+    {
+        // Display the value in this node.
+
+        std::cout << pCur->getItem();
+
+        // Move to the next node.
+        pCur = pCur->getNext();
     }
     std::cout << std::endl;
 }
@@ -140,22 +140,23 @@ void LinkedList<ItemType>::displayList() const
 // in the sorted linked list: if found, returns true
 // and copies the data in that node to the output parameter
 //**************************************************
-template <class ItemType>
-bool LinkedList<ItemType>::searchList(const ItemType* target, ItemType** dataOut) const
+template <class T>
+bool LinkedList<T>::searchList(const T *target, T **dataOut) const
 {
     bool found = false; // assume target not found
-    HashNode<ItemType> *pCur;         // To move through the list
-    
+    HashNode<T> *pCur;  // To move through the list
+
     // Position pCur: skip the head of the list.
     pCur = head->getNext();
     // Find location: skip all nodes whose name is less than target's name
-    while(pCur && pCur->getItem()->getID() < target->getID()){
+    while (pCur && pCur->getItem() < target)
+    {
         pCur = pCur->getNext();
     }
- 
- 
+
     // If found, copy data to the output parameter, and change the flag to true
-    if(pCur && pCur->getItem()->getID() == target->getID()){
+    if (pCur && pCur->getItem() == target)
+    {
         *dataOut = pCur->getItem();
         found = true;
     }
@@ -167,27 +168,27 @@ bool LinkedList<ItemType>::searchList(const ItemType* target, ItemType** dataOut
 // Destructor
 // This function deletes every node in the list.
 //**************************************************
-template <class ItemType>
-LinkedList<ItemType>::~LinkedList()
+template <class T>
+LinkedList<T>::~LinkedList()
 {
-    HashNode<ItemType> *pCur;     // To traverse the list
-    HashNode<ItemType> *pNext;    // To hold the address of the next node
-    
+    HashNode<T> *pCur;  // To traverse the list
+    HashNode<T> *pNext; // To hold the address of the next node
+
     // Position nodePtr: skip the head of the list
     pCur = head->getNext();
     // While pCur is not at the end of the list...
-    while(pCur)
+    while (pCur)
     {
         // Save a pointer to the next node.
         pNext = pCur->getNext();
-        
+
         // Delete the current node.
         delete pCur;
-        
-         // Position pCur at the next node.
+
+        // Position pCur at the next node.
         pCur = pNext;
     }
-    
+
     delete head; // delete the sentinel node
 }
 #endif /* LinkedList_h */
