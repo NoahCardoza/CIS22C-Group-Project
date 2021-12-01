5ed973913bb305fe81696c3bfc6d360a2144eda6
agarg.usa <agarg.usa@gmail.com>
2021-12-01 15:14:57 -0800

    added credits

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,25 @@
 
 int main(int argc, char *argv[])
 {
+	cout << "GENERAL INFORMATION" << endl;
+	cout << "====================" << endl;
+	cout << "This program is a database to hold covid-19 patients information / status." << endl;
+	cout << "It uses a Binary Search Tree and a Hashtable to store the data." << endl;
+	cout << "The program is able to add, delete, search, and print the data." << endl;
+	cout << "The program is able to save and load the data from a file." << endl;
+
+	cout << endl;
+
+	cout << "CREDITS" << endl;
+	cout << "=======" << endl;
+	cout << "This program was created by: " << endl;
+	cout << "Noah Cardoza - Team Leader, Worked on Team Coordination (Unit 1) and File I/O (Unit 5)" << endl;
+	cout << "Omar Hafud - Worked on Binary Search Tree (Unit 2)" << endl;
+	cout << "Sarina Karki - Worked on Hashtable (Unit 3)" << endl;
+	cout << "Aryan Garg - Worked on Screen I/O (Unit 4)" << endl;
+
+	cout << endl;
+
 	IOManager getIO = IOManager();
 
 	if (argc > 2)
